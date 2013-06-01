4672b4e15734271d27b8b7debe45ff99b8d21e0a
mingo <ipod825@gmail.com>
2013-06-01 14:27:10 +0800

    add moveCenter function

--- js/mystage.js
+++ js/mystage.js
@@ -1,18 +1,28 @@
 function MyStage(canvas, sourceManager){
 	Stage.call(this,canvas);
+	width=$(canvas).width();
+	height=$(canvas).height();
+	this.center=new Point(width/2,height/2);
 	this.sourceManager = sourceManager;
 	this.currentMap = null;
+	this.background=null;
 }
 
 
 MyStageProtoType={
 
 setCurrentMap : function(mapName){
-	this .currentMap = this.sourceManager.loadMap(mapName)
+	this.currentMap = this.sourceManager.loadMap(mapName)
 	this.removeAllChildren();
-	this.addChild(this.currentMap.image);
-}
+	this.background=this.currentMap.image;
+	this.addChild(this.background);
+},
 
+//move all other objects in the reverse direction according to center
+moveCenter : function(vX, vY){
+	this.background.x-=vX;
+	this.background.y-=vY;
+},
 
 }
 
