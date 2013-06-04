0832b1ac5cf934aecc58bdd2eb77840df1579a0b
mingo <ipod825@gmail.com>
2013-06-05 01:29:05 +0800

    setTarget done

--- js/mystage.js
+++ js/mystage.js
@@ -1,4 +1,4 @@
-function MyStage(canvas, sourceManager){
+function MyStage(canvas, sourceManager, player){
 	Stage.call(this,canvas);
 	width=$(canvas).width();
 	height=$(canvas).height();
@@ -7,6 +7,7 @@ function MyStage(canvas, sourceManager){
 	this.bgBox;
 	this.sourceManager = sourceManager;
 	this.currentMap = null;
+	this.player=player;
 }
 
 
@@ -20,10 +21,13 @@ MyStageProtoType={
 		this.bgBox=new MyRectangle(bg.x, bg.y, bg.image.width, bg.image.height);
 	},
 
-	isPassable : function(obj, p){
-		return this.currentMap.isPassable(obj, p.x, p.y);
+	//Check if a tile is movable. The unit of pos is in pixel.
+	//The object itself is passed to check the case that the object is just moving in the same tile(different pixel)
+	isPassable : function(obj, pos){
+		return this.currentMap.isPassable(obj, pos);
 	},
 
+	//Update the tiles when an object move
 	resetObjectPosition : function(obj,newP){
 		this.currentMap.resetObjectPosition(obj,newP);
 	},
@@ -32,9 +36,9 @@ MyStageProtoType={
 	moveOtherObjs: function(target, vX, vY){
 
 		//We move other objects in x or y direction when the target is at the center of the moving axis
-		if(vX!=0 && Math.abs(target.x-this.center.x)>target.step)
+		if(vX!=0 && !this.currentMap.sameTileIndex(target.x, this.center.x))
 			return false;
-		if(vY!=0 && Math.abs(target.y-this.center.y)>4)
+		if(vY!=0 && !this.currentMap.sameTileIndex(target.y, this.center.y))
 			return false;
 
 		//We do not move other objects if the viewBox is out of (intersect with) the background box
@@ -55,7 +59,8 @@ MyStageProtoType={
 	},
 
 	setTarget : function(p){
-
+		obj=this.currentMap.objectOnPos(p);
+		this.player.setTarget({"obj":obj,"pos":p});
 	}
 
 }
