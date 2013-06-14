f56ac89e21e8bee3784f9befaa60f22281fedbe9
sorrowsnow <slainblade@gmail.com>
2013-06-14 16:47:49 +0800

    Add Battle Support
    	modified:   js/attackableCharacter.js
    	new file:   js/battleManager.js
    	modified:   js/status.js

--- js/attackableCharacter.js
+++ js/attackableCharacter.js
@@ -14,81 +14,126 @@
  */
 function AttackableCharacter(battleManager, status)
 {
-	// For debugging
-	this.logger.verbose(this.tag, "AttackableCharacter: +++START+++ battleManager = " + battleManager + 
-	                    " , status = " + status);
+    // For debugging
+    this.logger.verbose(this.tag, "AttackableCharacter: +++START+++ battleManager = " + battleManager + 
+                        " , status = " + status);
 
-	// Call parent Consturctor
-	Character.call(this); 
+    // Call parent Consturctor
+    Character.call(this); 
 
-	// Initialize status and battleManager
-	if(status)
-	{ this.status = new Status(status); }  // New status with given status
-	else
-	{ this.status = new Status(); }        // New a status with default value
+    // Initialize status and battleManager
+    if(status)
+    { this.status = new Status(status); }  // New status with given status
+    else
+    { this.status = new Status(); }        // New a status with default value
 
-	if(battleManager) 
-	{ this.battleManager = battleManager; }
-	else
-	{
-		this.battleManager = null;         // The battle Manager reference
-		this.logger.error(this.tag, "AttackableCharacter: battleManager undefined");
-	}
+    if(battleManager) 
+    { this.battleManager = battleManager; }
+    else
+    {
+        this.battleManager = null;         // The battle Manager reference
+        this.logger.error(this.tag, "AttackableCharacter: battleManager undefined");
+    }
 };
 
 // Predefined attackable character prototype
 AttackableCharacterPrototype = 
 {
-	logger : new ConsoleLogger(),
-	tag : "[AttackableCharacter]: ",
+    logger : new ConsoleLogger(),
+    tag : "[AttackableCharacter]: ",
 
-	updateStatus : function(attrName, offset)
-	{
-		this.status.updateAttribute(attrName, offset);
-	},
+    // STATUS RELATED FUNCTIONS
+    // STATUS RELATED FUNCTIONS
+    // STATUS RELATED FUNCTIONS
 
-	useItem : function(item)
-	{
-		itemIndex = this.bag.indexOf(item);
-		if(itemIndex === -1)
-		{
-			// Not found
-			this.logger.error(this.tag, "useItem: item to use " + 
-			                  item[type] + " not found");
-			return;
-		}
+    getStatus: function()
+    {
+        if(!this.status)
+        {
+            this.logger.error(this.tag, "getStatus: status undefined");
+            return null;
+        }
+        return this.status;
+    },
 
-		// Valid item index, perform usage
-		if(this.bag[itemIndex].performUsage)
-		{
-			this.bag[itemIndex].performUsage(this.status);
-		}
-		else
-		{
-			this.logger.debug(this.tag, "useItem: this item is unuable");
-			// prevent from decreasing the number of item
-			return;
-		}
+    setStatus : function(attrName, value)
+    {
+        this.logger.verbose(this.tag, "setStatus: +++START+++ attrName = " +
+                            attrName + " , value = " + value);
+        this.status.setAttribute(attrName, value);
+    },
 
-		// Decrease the number of item
-		--(this.bag[itemIndex].number);
-		if(this.bag[itemIndex].number === 0)
-		{
-			// Remove that item from bag
-			this.bag.splice(itemIndex, 1);
-		}
-		else if(this.bag[itemIndex].number < 0)
-		{
-			this.logger.error(this.tag, "useItem: item number < 0," +
-			                  " check for reason");
-		}
-	},
+    updateStatus : function(attrName, offset)
+    {
+        this.logger.verbose(this.tag, "updateStatus: +++START+++ attrName = " +
+                            attrName + " , offset = " + offset);
+        this.status.updateAttribute(attrName, offset);
+    },
 
-	attack : function()
-	{
-		// TODO
-	}
+    // ITEM RELATED FUNCTIONS
 
+    useItem : function(item)
+    {
+        this.logger.verbose(this.tag, "useItem: +++START+++ item.type = " +
+                            item.type + " , item.number = " + item.number + 
+                            " , item.description = " + item.description);
+
+        itemIndex = this.bag.indexOf(item);
+        if(itemIndex === -1)
+        {
+            // Not found
+            this.logger.error(this.tag, "useItem: item to use " + 
+                              item[type] + " not found");
+            return;
+        }
+
+        // Valid item index, perform usage
+        if(this.bag[itemIndex].performUsage)
+        {
+            this.bag[itemIndex].performUsage(this.status);
+        }
+        else
+        {
+            this.logger.debug(this.tag, "useItem: this item is unuable");
+            // prevent from decreasing the number of item
+            return;
+        }
+
+        // Decrease the number of item
+        --(this.bag[itemIndex].number);
+        if(this.bag[itemIndex].number === 0)
+        {
+            // Remove that item from bag
+            this.bag.splice(itemIndex, 1);
+        }
+        else if(this.bag[itemIndex].number < 0)
+        {
+            this.logger.error(this.tag, "useItem: item number < 0," +
+                              " check for reason");
+        }
+    },
+
+    // ACTION RELATED FUNCTIONS
+
+    attack : function(attackee)
+    {
+        this.logger.verbose(this.tag, "attack: +++START+++ attackee = " + attackee);
+
+        // check needed members
+        if(!this.battleManager)
+        {
+            this.logger.error(this.tag, "attack: battleManager not linked");
+            return;
+        }
+        if(!attackee)
+        {
+            this.logger.error(this.tag, "attack: attackee undefined");
+            return;
+        }
+
+        // Call battle manager to handle damage computation
+        this.battleManager.performAttack(this, attackee);
+    }
 };
 
 
@@ -97,7 +142,7 @@ AttackableCharacter.prototype = new Character();
 // Assign the prototype to the predefined one
 for(var obj in AttackableCharacterPrototype)
 {
-	AttackableCharacter.prototype[obj] = AttackableCharacterPrototype[obj];
+    AttackableCharacter.prototype[obj] = AttackableCharacterPrototype[obj];
 }
 // Initialization of members in prototype
 AttackableCharacter.prototype.logger.setLogLevel("all");
---
+++ js/battleManager.js
@@ -0,0 +1,34 @@
+// Battle Class
+var Battle = function()
+{
+};
+
+// Prototype of battle
+Battle.prototype = 
+{
+    tag : "[BattlePrototype]: ",
+    logger : new ConsoleLogger(),
+
+    // Damage calculate funtion
+    performAttack(attacker, attackee)
+    {
+        var attackerStatus = attacker.getStatus();
+        var attackeeStatus = attackee.getStatus();
+        if(!attackerStatus)
+        {
+            this.logger.error(this.tag, "performAttack: attacker status not defined");
+            return;
+        }
+        if(!attackeeStatus)
+        {
+            this.logger.error(this.tag, "performAttack: attacker status not defined");
+            return;
+        }
+
+        var damage = attackerStatus.attack - attackeeStatus.defense;
+        attackee.updateStatus("hp", -damage);
+    }
+};
+
+// Initialize the member in prototype
+Battle.prototype.logger.setLogLevel("all");
